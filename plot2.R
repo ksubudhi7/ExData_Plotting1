7853941f8b8edb0f415d69a0d4c022c12fec8ce3
ksubudhi7 <57210581+ksubudhi7@users.noreply.github.com>
2020-01-18 07:13:41 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -7,14 +7,13 @@ datafile <- "./household_power_consumption.txt"
 data <- read.table(datafile, header=TRUE, sep=";", dec=".")
 #subset of the data applying a filter
 subset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-##date <- strptime(paste(subset$Date), "%d/%m/%Y") 
-##str(subset)
 #change the date and time format
 date <- strptime(paste(subset$Date,subset$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
 #check the date format
 str(date)
 #change the format of global active power 
 globalactivepower <- as.numeric(subset$Global_active_power)
+#check the format
 str(globalactivepower)
 #create a png output
 png("plot2.png",height = 480, width = 480)
